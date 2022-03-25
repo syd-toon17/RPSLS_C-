d5f6679396282ad18312de04c50cdc64329de6e9
Sydney Wilson <syd.toon17@gmail.com>
2022-03-25 16:35:14 -0500

    Started workin on rpsls project.

---
+++ RPSLS/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Player
+    {
+        public int wins;
+        public string name;
+        public string chosenGesture;
+        List<string> gestures = new List<string> ();
+        gestures.Add("Rock");
+    }
+}
---
+++ RPSLS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RPSLS // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player one = new Player();
+
+            one.name = "sydney";
+            
+            one.chosenGesture = "rock";
+            one.wins = 3;
+
+            Console.WriteLine(one.name + ' ' + one.chosenGesture);
+            Console.ReadLine();
+
+        }
+    }
+}
